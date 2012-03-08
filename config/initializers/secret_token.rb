d934a6baeaa4ad44b740e49189312bccc9574873
mark <mhurwitzus@gmail.com>
2012-03-08 09:58:17 -0600

    initialized a boilerplate static page app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Losmarco::Application.config.secret_token = '7a66b08331645f5be99de7f2da7775cb9c3c3345af01a095f3e2b725602e5ef46194888ede082b9761051a912ae5a893a512458ef0a27db4429b5757dddee616'
